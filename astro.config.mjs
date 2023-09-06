57af0c4e5f654a98307d700ceca1dab55da4adfe
njshockey <shockeynj@gmail.com>
2023-09-06 16:45:54 -0400

    added prettier, eslint, and editorconfig
    Also ran prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+	integrations: [tailwind(), sitemap()],
 });
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,7 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  plugins: {
-    autoprefixer: {},
-  },
+	plugins: {
+		autoprefixer: {},
+	},
 };
