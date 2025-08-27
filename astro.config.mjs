afc97716757ba9b180f25beaa4666ea97ac123c0
njs-guy <shockeynj@gmail.com>
2025-08-27 15:19:33 -0400

    Changed light shiki theme to the one-light that's already included

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 import remarkToc from "remark-toc";
 
-import OneLight from "./shiki-themes/OneLight.json";
-
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), sitemap()],
@@ -16,11 +14,8 @@ export default defineConfig({
 	output: "static",
 	markdown: {
 		shikiConfig: {
-			// Temporary One Light theme until it's added into Astro.
-			// Shiki Textmate Grammar Themes - https://github.com/shikijs/textmate-grammars-themes/tree/main/packages/tm-themes
-			// One Light theme by akamud - https://github.com/akamud/vscode-theme-onelight/blob/f1ff6b87c6379a22b34354bbf64d355cd2adc611/themes/OneLight.json#L1
 			themes: {
-				light: OneLight,
+				light: "one-light",
 				dark: "one-dark-pro",
 			},
 		},
